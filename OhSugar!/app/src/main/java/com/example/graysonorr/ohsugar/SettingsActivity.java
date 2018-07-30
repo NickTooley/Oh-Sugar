42ba0fb91c9d5a3ad220466b275874d5fbcb7116
Nicholas Tooley (1000029967) <TOOLNJ1@student.op.ac.nz>
2018-07-30 18:06:35 +1200

    Added a lot of GUI elements, completed welcome screen as well as some settings screen features

--- OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/SettingsActivity.java
+++ OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/SettingsActivity.java
@@ -1,13 +1,57 @@
 package com.example.graysonorr.ohsugar;
 
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.Typeface;
 import android.os.*;
 import android.support.v7.app.*;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        TextView toolBarTitle = findViewById(R.id.toolbar_title);
+        Typeface customFont = Typeface.createFromAsset(getAssets(), getString(R.string.font));
+        toolBarTitle.setTypeface(customFont);
+
+        Button conversionsBtn = (Button) findViewById(R.id.button);
+
+        Toolbar mActionBar = (Toolbar) findViewById(R.id.toolbar_top);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mActionBar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp));
+            mActionBar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //What to do on back clicked
+                }
+            });
+
+        }
+
+
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
+
+
--- OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/WelcomeActivity.java
+++ OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/WelcomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.graysonorr.ohsugar;
 
+import android.content.Intent;
 import android.graphics.*;
 import android.os.*;
 import android.support.v7.app.*;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.*;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -14,5 +18,31 @@ public class WelcomeActivity extends AppCompatActivity {
         TextView toolBarTitle = findViewById(R.id.toolbar_title);
         Typeface customFont = Typeface.createFromAsset(getAssets(), getString(R.string.font));
         toolBarTitle.setTypeface(customFont);
+
+        //Shader txtShader = new LinearGradient(0,0,100,0, new int[]{R.color.colorTomato, R.color.colorDeepPink}, new float[]{0,1}, Shader.TileMode.CLAMP);
+        //toolBarTitle.getPaint().setShader(txtShader);
+
+        // Code snippet to change notification bar color
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorLightSlateGray));
+        }
+        //Snippet end
+
+        Button settingsBtn = (Button) findViewById(R.id.settingsBtn);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
