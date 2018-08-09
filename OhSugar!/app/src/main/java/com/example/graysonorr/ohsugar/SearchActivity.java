31117875df6b6629007163713d374ab042b96f33
Nicholas Tooley (1000029967) <TOOLNJ1@student.op.ac.nz>
2018-08-09 17:06:51 +1200

    Beginning process of adding search functionality, layout completed

--- OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/SearchActivity.java
+++ OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/SearchActivity.java
@@ -1,11 +1,16 @@
 package com.example.graysonorr.ohsugar;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
@@ -25,5 +30,26 @@ public class SearchActivity extends AppCompatActivity {
         TextView toolBarTitle = findViewById(R.id.toolbar_title);
         Typeface customFont = Typeface.createFromAsset(getAssets(), getString(R.string.font));
         toolBarTitle.setTypeface(customFont);
+
+
+        EditText searchText = (EditText) findViewById(R.id.searchText);
+
+        searchText.measure(0,0);
+
+        Shader textShader2=new LinearGradient(0, 0, searchText.getMeasuredWidth() / 2, 0,
+                new int[]{Color.parseColor("#fc552e"),Color.parseColor("#f9398e")},
+                new float[]{0, 1}, Shader.TileMode.CLAMP);
+        searchText.getPaint().setShader(textShader2);
+
+
+        searchText.setImeActionLabel("Search", KeyEvent.KEYCODE_ENTER);
+
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                return false;
+            }
+        });
+
     }
 }
