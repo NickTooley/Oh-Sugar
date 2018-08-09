6a225fe11de1691b3a1035920a52e127d8d9d22f
Nicholas Tooley (1000029967) <TOOLNJ1@student.op.ac.nz>
2018-08-09 18:07:22 +1200

    Added barcode scanner to the search app , retrieves barcode but does nothing with the information

--- OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/SearchActivity.java
+++ OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.graysonorr.ohsugar;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
@@ -7,13 +8,21 @@ import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.graysonorr.ohsugar.db.AppDatabase;
+import com.example.graysonorr.ohsugar.db.Food;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,7 @@ public class SearchActivity extends AppCompatActivity {
         Typeface customFont = Typeface.createFromAsset(getAssets(), getString(R.string.font));
         toolBarTitle.setTypeface(customFont);
 
+        db = AppDatabase.getInMemoryDatabase(getApplicationContext());
 
         EditText searchText = (EditText) findViewById(R.id.searchText);
 
@@ -41,15 +51,44 @@ public class SearchActivity extends AppCompatActivity {
                 new float[]{0, 1}, Shader.TileMode.CLAMP);
         searchText.getPaint().setShader(textShader2);
 
-
-        searchText.setImeActionLabel("Search", KeyEvent.KEYCODE_ENTER);
-
         searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 return false;
             }
         });
+
+        Button scanBtn = (Button) findViewById(R.id.scanBtn);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchActivity.this, BarcodeScanner.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String currentQRCode;
+        if (requestCode == 1) {
+            currentQRCode = data.getStringExtra("nada");
+            if(currentQRCode!=null) {
+                fetchBarcodeData(currentQRCode);
+            }
+        }
+    }
+
+    private void fetchBarcodeData(String barcode) {
+        // This activity is executing a query on the main thread, making the UI perform badly.
+        Log.d("Barcode", barcode);
+        Food food = db.foodDao().findByBarcode(barcode);
+
+        if(food != null){
+            //showOutput(food);
+        }
 
     }
 }
