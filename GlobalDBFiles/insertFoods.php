d4b078af053f5d65ddc9f46594beea283ce352cd
Nicholas Tooley (1000029967) <TOOLNJ1@student.op.ac.nz>
2018-08-30 13:21:21 +1200

    Updated Insertion PHP file

--- GlobalDBFiles/insertFoods.php
+++ GlobalDBFiles/insertFoods.php
@@ -7,15 +7,17 @@ session_start();?>
 <?php
   $rest_json = file_get_contents("php://input");
   $_POST = json_decode($rest_json, true);
-  
   if (isset($_POST['foodInsert'])){
     $date = date('Y-m-d H:i:s');
 
 
-    $insertFood = $pdo->prepare("INSERT INTO Foods (foodName, sugar, barcode, category, dateAdded) VALUES(:foodname, :sug, :barc, :categ, :date)");
+    $insertFood = $pdo->prepare("INSERT INTO Foods (foodName, sugarPerServe, sugarPer100, barcode, category, dateAdded) 
+									VALUES(:foodname, :sug1, :sug2, :barc, :categ, :date)
+									ON DUPLICATE KEY UPDATE sugarPerServe = :sug1, sugarPer100 = :sug2, category = :categ, dateAdded = :date");
 	
 	$insertFood->bindParam(':foodname',$foodName);
-    $insertFood->bindParam(':sug',$sugar);
+    $insertFood->bindParam(':sug1',$sugar1);
+    $insertFood->bindParam(':sug2',$sugar2);
     $insertFood->bindParam(':barc', $barcode);
     $insertFood->bindParam(':categ', $category);
     $insertFood->bindParam(':date', $date);
@@ -30,7 +32,7 @@ session_start();?>
 		echo("\n");
 		print_r($foods[$i]['name']);
 		echo("\n");
-		print_r($foods[$i]['sugar']);
+		print_r($foods[$i]['sugar1']);
 		echo("\n");
 		print_r($foods[$i]['barcode']);
 		echo("\n");
@@ -38,13 +40,34 @@ session_start();?>
 		echo("\n");
 		
 		$foodName = $foods[$i]['name'];
-		$sugar = $foods[$i]['sugar'];
+		$sugar1 = $foods[$i]['sugar1'];
+		$sugar2 = $foods[$i]['sugar2'];
 		$barcode = $foods[$i]['barcode'];
 		$category = $foods[$i]['category'];
 		
 		$insertFood->execute();
 		echo("Food INSERTED\n \n");
 	}
+	
+	/**
+	$foodjson = $_POST['foods'];
+	$foodarr = json_decode($foodjson, true);
+	**/
+	
+	
+	/**
+	echo $foodarr;
+	**/
+	
+	
+	/**
+	for($foodarr as $key => $value){
+		$foodname = $_POST['name'];
+		$sugar = $_POST['sugar'];
+		$barcode = $_POST['barcode'];
+		$category = $_POST['category'];
+	}
+    **/
   }
 
     
