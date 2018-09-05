8a07c19e477175c8841485ba6460a69ff4d6a393
caincb1 <caincb1@student.op.ac.nz>
2018-09-05 22:13:28 +1200

    Performance activity started

--- OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/HealthActivity.java
+++ OhSugar!/app/src/main/java/com/example/graysonorr/ohsugar/HealthActivity.java
@@ -5,11 +5,18 @@ import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HealthActivity extends AppCompatActivity {
+
+    ImageView arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,28 @@ public class HealthActivity extends AppCompatActivity {
         Typeface customFont = Typeface.createFromAsset(getAssets(), getString(R.string.font));
         toolBarTitle.setTypeface(customFont);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("Family", MODE_PRIVATE);
+
+        arrow = (ImageView) findViewById(R.id.arrow);
+        float percentage = ((((float)193/(float)sharedPreferences.getInt("familySugar", 0))*100)/180)*100;
+
+        rotateNeedle(0, Math.round(percentage));
+
         TextView recSugar = (TextView) findViewById(R.id.RecSugarTotal);
         TextView listSugar = (TextView) findViewById(R.id.ListSugarTotal);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("Family", MODE_PRIVATE);
         recSugar.setText("Rec Sugar: " + Integer.toString(sharedPreferences.getInt("familySugar", 0)) + "g");
+        listSugar.setText(Float.toString(percentage));
+    }
+
+    public void rotateNeedle(int startPoint, int endPoint){
+        RotateAnimation rotateAnimation1 = new RotateAnimation(startPoint, endPoint,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation1.setInterpolator(new LinearInterpolator());
+        rotateAnimation1.setDuration(1000);
+        rotateAnimation1.setFillAfter(true);
+        arrow.startAnimation(rotateAnimation1);
     }
 }
